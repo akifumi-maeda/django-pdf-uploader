56e4164804ede169184218f1fad5bb13b7e586e1
Akifumi Maeda <akifumi_maeda@maede.co.jp>
2021-07-16 03:54:39 +0000

    Set db

--- pdfuploader/settings.py
+++ pdfuploader/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-42=)apzch2++g_(ygeg&71sb)kue1psgm^xmx6rvcxyk%6jbph'
+# SECRET_KEY = 'django-insecure-42=)apzch2++g_(ygeg&71sb)kue1psgm^xmx6rvcxyk%6jbph'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-42=)apzch2++g_(ygeg&71sb)kue1psgm^xmx6rvcxyk%6jbph')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
